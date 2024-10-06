36cf7dd735858c306e82dbe9b2fd6b58f4de06b9
Erwin Darsono <erwindarsono08@gmail.com>
2024-10-06 20:48:50 +0700

    receive file from server OK

--- src/main/java/org/example/ClientEcho.java
+++ src/main/java/org/example/ClientEcho.java
@@ -1,11 +1,16 @@
 package org.example;
 
 import jakarta.websocket.ClientEndpoint;
+import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.nio.ByteBuffer;
 
 @ClientEndpoint
 public class ClientEcho {
@@ -15,7 +20,29 @@ public class ClientEcho {
             InetAddress me = InetAddress.getLocalHost();
             String dottedSquad = me.getHostAddress();
             session.getBasicRemote().sendText(dottedSquad);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Type yes to receive file from server");
+            String command = reader.readLine();
+            session.getBasicRemote().sendText(command);
         } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @OnMessage
+    public void onMessage(ByteBuffer message){
+        try {
+            System.out.println("From server : ");
+            // Save the received file to disk
+            byte[] data = new byte[message.remaining()];
+            message.get(data);
+
+            // Save to a file (e.g., received_file.pdf)
+            try (FileOutputStream fos = new FileOutputStream("received_file.txt")) {
+                fos.write(data);
+                System.out.println("File saved as received_file.pdf.");
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/org/example/DocClient.java
+++ src/main/java/org/example/DocClient.java
@@ -4,11 +4,20 @@ import jakarta.websocket.Session;
 import org.glassfish.tyrus.client.ClientManager;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.Socket;
+import java.net.SocketException;
 import java.net.URI;
+import java.util.Collections;
+import java.util.Enumeration;
 
 public class DocClient{
     public static void main(String[] args) throws Exception{
         ClientManager client = ClientManager.createClient();
-        client.connectToServer(ClientEcho.class, new URI("ws://localhost:8025/websockets/echo"));
+        while(true){
+            client.connectToServer(ClientEcho.class, new URI("ws://192.168.0.106:8025/websockets/echo"));
+        }
     }
 }
