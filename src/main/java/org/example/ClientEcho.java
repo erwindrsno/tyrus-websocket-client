253105c2060b58574ecd2fc30a3b52a04e4f5db0
Erwin Darsono <erwindarsono08@gmail.com>
2024-10-15 21:58:38 +0700

    receive chunk files OK

--- src/main/java/org/example/ClientEcho.java
+++ src/main/java/org/example/ClientEcho.java
@@ -5,15 +5,19 @@ import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.OnClose;
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.*;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @ClientEndpoint
 public class ClientEcho {
+    Path sourcePath = Paths.get("toBeReceived");
+    FileOutputStream fos;
     @OnOpen
     public void onOpen(Session session){
         try {
@@ -24,29 +28,46 @@ public class ClientEcho {
             //System.out.println("Type yes to receive file from server");
             //String command = reader.readLine();
             //session.getBasicRemote().sendText(command);
+            fos = new FileOutputStream(sourcePath.toFile());
         } catch (Exception e){
             e.printStackTrace();
         }
     }
 
     @OnMessage
-    public void onMessage(ByteBuffer message){
+    public void onMessage(ByteBuffer message, boolean isLast){
         try {
+            int idx = 1;
+            String strIdx = idx + "";
             //Save the received file to disk
             byte[] data = new byte[message.remaining()];
+            System.out.println("Remaining : " + strIdx + " " + message.remaining());
             message.get(data);
             //Save to a file (e.g., received_file.pdf)
-            try (FileOutputStream fos = new FileOutputStream("received_file.txt")) {
-                fos.write(data);
-                System.out.println("File saved as received_file.txt");
+
+            fos.write(data);
+
+            if(isLast){
+                System.out.println("File transferred!");
+                fos.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    @OnMessage
+    public void renameFile(String fileName){
+        Path targetPath = Paths.get(fileName);
+        try{
+            Files.move(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @OnClose
     public void onClose(){
-        System.out.println("Connection closedeafvesdbhrewhgnvijinulh");
+        System.out.println("Connection closed by client");
     }
 }
